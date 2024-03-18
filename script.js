dba141b70510c183258b65947537cf0ea89dcc10
Jayant_Nayan <jayantnayan3@gmail.com>
2024-03-18 23:30:35 +0530

    Coffee_Cafe

---
+++ script.js
@@ -0,0 +1,18 @@
+var sidemeu = document.getElementById("nav-menu");
+
+      function openmenu() {
+        sidemeu.style.right = "0";
+      }
+      function closemenu() {
+        sidemeu.style.right = "-200px";
+      }
+
+      var cardmenu = document.querySelector(".card-menu");
+
+
+      function cardsMenusToggle(){
+        cardmenu.classlist(".card-menu-height");
+
+
+
+      }
